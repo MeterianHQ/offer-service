7678b21eae693bd8d587f145487696201b0240e4
roman-khomyshyn <roman.khomyshyn@ovoenergy.com>
2018-02-23 17:20:33 +0200

    Added all arguments constructor for offer redeem info dto.

--- src/main/java/com/ovoenergy/offer/dto/OfferRedeemInfoDTO.java
+++ src/main/java/com/ovoenergy/offer/dto/OfferRedeemInfoDTO.java
@@ -1,11 +1,13 @@
 package com.ovoenergy.offer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class OfferRedeemInfoDTO {
     private String email;
