241d3d585d09c2a95dca22941c9340636fffbb10
yuliya-makarchuk <yuliya.makarchuk@ovoenergy.com>
2018-02-23 15:51:23 +0200

    ODE-85: Update status by custom query in one transaction

--- src/main/java/com/ovoenergy/offer/db/repository/OfferRepository.java
+++ src/main/java/com/ovoenergy/offer/db/repository/OfferRepository.java
@@ -3,7 +3,11 @@ package com.ovoenergy.offer.db.repository;
 import com.ovoenergy.offer.db.entity.OfferDBEntity;
 import com.ovoenergy.offer.db.entity.StatusType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<OfferDBEntity, Long> {
@@ -17,4 +21,10 @@ public interface OfferRepository extends JpaRepository<OfferDBEntity, Long> {
     OfferDBEntity findOneByOfferCodeIgnoreCase(String offerCode);
 
     List<OfferDBEntity> findAllByStatusAndExpiryDateLessThan(StatusType status, Long expiryDate);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE offer o SET o.status = 'EXPIRED', o.updatedOn = :now WHERE o.expiryDate < :nowMidnight AND o.status = 'ACTIVE'")
+    int updateExpiredOffersStatus(@Param("now") Long now, @Param("nowMidnight") Long nowMidnight);
+
 }
